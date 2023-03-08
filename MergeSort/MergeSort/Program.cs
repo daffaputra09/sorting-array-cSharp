5f824dea26551da32222dcd51794d7e8e240713e
daffaputra09 <daffaputra863@gmail.com>
2023-03-08 10:51:25 +0700

    update

--- MergeSort/MergeSort/Program.cs
+++ MergeSort/MergeSort/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 
+
 namespace MergeSort
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            int[] list = new int[] { 7, 1, 4, 3, 6, 2, 5 };
-            MergeSort(list);
+            int[] nilai = { 7, 1, 4, 3, 6, 2, 5 };
+            MergeSort(nilai);
         }
-        private static void MergeSort(int[] number)
+
+
+        public static void MergeSort(int[] number)
         {
             Console.WriteLine("===BEFORE===");
             foreach (var n in number)
@@ -17,14 +20,101 @@ namespace MergeSort
                 Console.Write(n + ", ");
             }
 
-
-
-
+            number = PisahArray(number);
             Console.WriteLine("\n===AFTER===");
             foreach (var n in number)
             {
                 Console.Write(n + ", ");
             }
+        }
+
+ 
+        public static int[] PisahArray(int[] number)
+        {
+    
+            if (number.Length <= 1)
+            {
+       
+                return number;
+            }
+            int tengah = number.Length / 2;
+            int[] kiri = new int[tengah];
+            int[] kanan;
+
+       
+            if (number.Length % 2 == 0)
+            {
+                kanan = new int[tengah];
+            }
+            else
+            {
+                kanan = new int[tengah + 1];
+            }
+
+   
+            for (int i = 0; i < kiri.Length; i++)
+            {
+                kiri[i] = number[i];
+            }
+
+ 
+            for (int i = 0; i < kanan.Length; i++)
+            {
+                kanan[i] = number[tengah + i];
+            }
+
+
+            kiri = PisahArray(kiri);
+            kanan = PisahArray(kanan);
+
+            int[] hasil = new int[number.Length];
+
+            hasil = GabungArray(kiri, kanan);
+
+            return hasil;
+        }
+
+        public static int[] GabungArray(int[] kiri, int[] kanan)
+        {
+            int[] hasilGabung = new int[kiri.Length + kanan.Length];
+
+            int indexKiri = 0, indexKanan = 0, indexHasilGabung = 0;
+
+            while (indexKiri < kiri.Length || indexKanan < kanan.Length)
+            {
+                if (indexKiri < kiri.Length && indexKanan < kanan.Length)
+                {
+                    if (kiri[indexKiri] < kanan[indexKanan])
+                    {
+                        hasilGabung[indexHasilGabung] = kiri[indexKiri];
+                        indexHasilGabung++;
+                        indexKiri++;
+                    }
+                    else
+                    {
+                        hasilGabung[indexHasilGabung] = kanan[indexKanan];
+                        indexHasilGabung++;
+                        indexKanan++;
+                    }
+
+                }
+                else if (indexKiri < kiri.Length)
+                {
+
+                    hasilGabung[indexHasilGabung] = kiri[indexKiri];
+                    indexHasilGabung++;
+                    indexKiri++;
+                }
+                else if (indexKanan < kanan.Length)
+                {
+                    hasilGabung[indexHasilGabung] = kanan[indexKanan];
+                    indexHasilGabung++;
+                    indexKanan++;
+                }
+            }
+
+
+            return hasilGabung;
         }
     }
 }
